a4748289788cdab65d19e40f744d30348df0284b
Jeremy Caine <jezcaine@gmail.com>
2021-01-13 09:56:50 +1100

    updates

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 var http = require('http');
 
+const port = 3000;
+
 var server = http.createServer(function(req, res) {
+  console.log("ping");
   res.writeHead(200);
   res.end('Hello World !');
 });
-server.listen(8080);
+//server.listen(port);
 
-/*
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
